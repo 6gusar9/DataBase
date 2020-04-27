cebf006b8eebe33ad7c706be687ddb256b95725f
Детеедъ <58175410+6gusar9@users.noreply.github.com>
2020-04-27 12:24:49 +0300

    1

--- Base/Form1.Designer.vb
+++ Base/Form1.Designer.vb
@@ -26,7 +26,12 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.Grid2 = New System.Windows.Forms.DataGridView()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
         CType(Me.Grid1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Grid2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Grid1
@@ -34,12 +39,12 @@ Partial Class Form1
         Me.Grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.Grid1.Location = New System.Drawing.Point(12, 12)
         Me.Grid1.Name = "Grid1"
-        Me.Grid1.Size = New System.Drawing.Size(530, 326)
+        Me.Grid1.Size = New System.Drawing.Size(408, 326)
         Me.Grid1.TabIndex = 0
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(39, 379)
+        Me.Button1.Location = New System.Drawing.Point(12, 353)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(122, 59)
         Me.Button1.TabIndex = 1
@@ -48,7 +53,7 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(185, 379)
+        Me.Button2.Location = New System.Drawing.Point(163, 353)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(115, 59)
         Me.Button2.TabIndex = 2
@@ -57,18 +62,57 @@ Partial Class Form1
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(328, 379)
+        Me.Button3.Location = New System.Drawing.Point(313, 353)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(107, 59)
         Me.Button3.TabIndex = 3
         Me.Button3.Text = "Удалить"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'Grid2
+        '
+        Me.Grid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Grid2.Location = New System.Drawing.Point(427, 13)
+        Me.Grid2.Name = "Grid2"
+        Me.Grid2.Size = New System.Drawing.Size(423, 325)
+        Me.Grid2.TabIndex = 4
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(738, 353)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(107, 59)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Удалить"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(588, 353)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(115, 59)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "Добавить"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(437, 353)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(122, 59)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "Обновить"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(554, 450)
+        Me.ClientSize = New System.Drawing.Size(862, 450)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Grid2)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
@@ -76,6 +120,7 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.Text = " "
         CType(Me.Grid1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Grid2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -84,4 +129,8 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
+    Friend WithEvents Grid2 As DataGridView
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
 End Class
--- Base/Form1.vb
+++ Base/Form1.vb
@@ -5,15 +5,16 @@ Public Class Form1
     End Sub
     Private Sub RefreshGrid()
 
-        Dim c As New OleDbCommand
-        c.Connection = conn
-        c.CommandText = "select * from Группа"
+        'Dim c As New OleDbCommand
+        ' c.Connection = conn
+        ' c.CommandText = "select * from Группа"
 
-        Dim ds As New DataSet
-        Dim da As New OleDbDataAdapter(c)
-        da.Fill(ds, "Группа")
-        Grid1.DataSource = ds
-        Grid1.DataMember = "Группа"
+        ' Dim ds As New DataSet
+        ' Dim da As New OleDbDataAdapter(c)
+        ' da.Fill(ds, "Группа")
+        ' Grid1.DataSource = ds
+        'Grid1.DataMember = "Группа"
+        FillGrid(Grid1, "select * from Группа", "Группа")
 
     End Sub
 
@@ -25,10 +26,24 @@ Public Class Form1
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim s1, s2, s3 As String
+        Dim r As DialogResult
+        Form2.ShowDialog()
+        s1 = Form2.TextBox1.Text
+        s2 = Form2.TextBox2.Text
+        s3 = Form2.TextBox3.Text
+        r = Form2.DialogResult
+        Form2.Close()
+
+        If r <> DialogResult.OK Then
+            Exit Sub
+        End If
+
         Dim c As New OleDbCommand
         c.Connection = conn
-        c.CommandText = "insert into Группа(ФИО, Телефон, email) values('Студент','21212121','почта')"
+        c.CommandText = "insert into Группа(ФИО, Телефон, email) values('" & s1 & "','" & s2 & "','" & s3 & "')"
         c.ExecuteNonQuery()
+
         RefreshGrid()
     End Sub
 
@@ -37,9 +52,18 @@ Public Class Form1
         Dim c As New OleDbCommand
         c.Connection = conn
         k = Grid1.CurrentRow.Cells("ID").Value
-        c.CommandText = "detele from Группа where ID = " & k
+        c.CommandText = "delete from Группа where ID = " & k
         c.ExecuteNonQuery()
         RefreshGrid()
 
+    End Sub
+
+    Private Sub Grid1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles Grid1.CellContentClick
+        RefreshGrid2()
+    End Sub
+    Private Sub RefreshGrid2()
+        Dim k As Integer
+        k = Grid1.CurrentRow.Cells("ID").Value
+        FillGrid(Grid2, "select * from Событие where ID_Группа = " & k, "Группа")
     End Sub
 End Class
